850f527c284c598e9fa9f202d7104fed49a7a15f
bop-bop <alexander.kromer@student.hpi.de>
2022-09-20 18:40:30 +0200

    removed duplicate folder and zip

--- MasterThesis/code/masterThesis/training/alpha_scheduler.py
+++
@@ -1,7 +0,0 @@
-class alpha_scheduler:
-    def __init__(self, alpha_max, alpha_min, num_epochs):
-        self.alpha_max = alpha_max
-        self.alpha_min = alpha_min
-        self.num_epochs = num_epochs
-
-    #def current_alpha(self, current_epoch):
